df87926c1e669c66258de466243dd48034f26557
Sigurd Farstad Iversen <sigurdfarstad@gmail.com>
2023-07-26 00:01:15 +0100

    color stuff
    ???maybe

--- particles.js
+++ particles.js
@@ -27,7 +27,7 @@ var pJS = function(tag_id, params){
         }
       },
       color: {
-        value: '#fff'
+        value: '#69fff5'
       },
       shape: {
         type: 'circle',
@@ -67,7 +67,7 @@ var pJS = function(tag_id, params){
       line_linked: {
         enable: true,
         distance: 100,
-        color: '#fff',
+        color: '#69fff5',
         opacity: 1,
         width: 1
       },
